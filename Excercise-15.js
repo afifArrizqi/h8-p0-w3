e481fbac3330b04c37c480873a0012dfdc676730
pippop21 <afifar38@gmail.com>
2020-03-06 16:34:24 +0700

    Exercise-15 use Flagging

--- Excercise-15.js
+++ Excercise-15.js
@@ -1,22 +1,20 @@
 function groupAnimals(animals) {
 	// you can only write your code here!
 	var newAnimals = [];
-	var character = [];
-	// GROUPING
-	for (var i = 0; i < animals.length; i++) {
-		for (var j = 0; j <= character.length; j++) {
-			if (animals[i][0] === character[j]) {
+	for (let i = 0; i < animals.length; i++) {
+		var check = false;
+		for (var j = 0; j < newAnimals.length; j++) {
+			if (newAnimals[j][0][0] === animals[i][0]) {
+				check = true;
 				newAnimals[j].push(animals[i]);
-				break;
-			} else if (j === character.length) {
-				character.push(animals[i][0]);
-				newAnimals.push([animals[i]]);
-				break;
 			}
+		}
+		if (!check) {
+			newAnimals.push([animals[i]]);
 		}
 	}
 
-	// SHORTING
+	// SORTING
 	for (var i = 0; i < newAnimals.length; i++) {
 		for (var j = 0; j < newAnimals.length - 1; j++) {
 			var a = newAnimals[j][0][0];
@@ -36,6 +34,3 @@ console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
 // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak']));
 // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
-
-//URUTKAN
-//LOOP UNTIL MEET DIFFERENT CHAR OR END OF LENGTH THEN PUSH
