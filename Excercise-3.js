20e7713352a6999fe51cf1bfa2ed08c94e5870c1
pippop21 <afifar38@gmail.com>
2020-03-06 20:08:57 +0700

    Exercise-2.js

--- Excercise-3.js
+++ Excercise-3.js
@@ -1,16 +1,20 @@
 //contoh input
 var input = [
-	[ '0001', 'Roman Alamsyah', 'Bandar Lampung', '21/05/1989', 'Membaca' ],
-	[ '0002', 'Dika Sembiring', 'Medan', '10/10/1992', 'Bermain Gitar' ],
-	[ '0003', 'Winona', 'Ambon', '25/12/1965', 'Memasak' ],
-	[ '0004', 'Bintang Senjaya', 'Martapura', '6/4/1970', 'Berkebun' ]
+	['0001', 'Roman Alamsyah', 'Bandar Lampung', '21/05/1989', 'Membaca'],
+	['0002', 'Dika Sembiring', 'Medan', '10/10/1992', 'Bermain Gitar'],
+	['0003', 'Winona', 'Ambon', '25/12/1965', 'Memasak'],
+	['0004', 'Bintang Senjaya', 'Martapura', '6/4/1970', 'Berkebun']
 ];
 
-for (var i = 0; i < input.length; i++) {
-	console.log(`
-	Nomor ID:  ${input[i][0]}
-	Nama Lengkap:  ${input[i][1]}
-	TTL:  ${input[i][2]} ${input[i][3]}
-	Hobi:  ${input[i][4]}
-	`);
+function dataHandling(input) {
+	for (var i = 0; i < input.length; i++) {
+		console.log(`
+		Nomor ID:  ${input[i][0]}
+		Nama Lengkap:  ${input[i][1]}
+		TTL:  ${input[i][2]} ${input[i][3]}
+		Hobi:  ${input[i][4]}
+		`);
+	}
 }
+
+console.log(dataHandling(input))
